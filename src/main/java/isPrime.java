e6886817d3db6502ec1ccd419a45c5854c564e49
Josiahngugi <mainajosiahdev@gmail.com>
2019-01-28 01:11:22 -0800

    if number is less than 1 return false

--- src/main/java/isPrime.java
+++ src/main/java/isPrime.java
@@ -1,4 +1,12 @@
 public class isPrime {
+    static boolean isPrime(int n){
+
+      if( n<=1) {
+          return false;
+      }
+
+    return true;
+    }
     public static void main(String [] args){
 
     }

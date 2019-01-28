2d36080f8a0299eaf7abbc7deb574e539572f236
Josiahngugi <mainajosiahdev@gmail.com>
2019-01-28 01:12:34 -0800

    if number is divisible by 2 return false

--- src/main/java/isPrime.java
+++ src/main/java/isPrime.java
@@ -5,6 +5,10 @@ public class isPrime {
           return false;
       }
 
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
     return true;
     }
     public static void main(String [] args){
